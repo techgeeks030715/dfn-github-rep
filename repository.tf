5c562717d7cda85c271f5e07869ee45b05b01a33
techgeeks030715 <techgeeks070315@gmail.com>
2023-05-14 17:37:00 +0530

    list of branches per repo to apply branch protection to them

--- repository.tf
+++ repository.tf
@@ -3,7 +3,7 @@ resource "github_repository" "default" {
 
   name = each.key
 
-  visibility             = "private"
+  visibility             = "public"
   has_issues             = false
   has_discussions        = false
   has_projects           = false
@@ -27,34 +27,16 @@ resource "github_repository" "default" {
 #   branch     = github_branch.development[each.key].branch
 # }
 
-resource "github_branch_protection" "prd" {
-  for_each = github_repository.default
-
-  repository_id = each.value.name
-  pattern       = "planned-release-dev"
-
-  required_pull_request_reviews {
-    required_approving_review_count = 2
-    require_code_owner_reviews      = true
-    require_last_push_approval      = true
-    #pull_request_bypassers = var.pull_request_bypassers
-  }
-
-  required_status_checks {
-    strict   = true
-    contexts = ["build-and-test-ubuntu"]
-  }
-
-  require_conversation_resolution = true
-  lock_branch                     = true
+data "github_repository_branches" "example" {
+    for_each   =  github_repository.default
+    repository = each.key
 }
 
-
-resource "github_branch_protection" "master" {
+resource "github_branch_protection" "example" {
   for_each = github_repository.default
 
   repository_id = each.value.name
-  pattern       = "master"
+  pattern       = var.branch_pattern[0]
 
   required_pull_request_reviews {
     required_approving_review_count = 2
@@ -63,29 +45,51 @@ resource "github_branch_protection" "master" {
     #pull_request_bypassers = var.pull_request_bypassers
   }
 
-  required_status_checks {
-    strict   = true
-    contexts = ["build-and-test-ubuntu"]
-  }
+ required_status_checks {
+   strict   = true
+   contexts = ["build-and-test-ubuntu"]
+ }
 
-  require_conversation_resolution = true
-  lock_branch                     = true
+ require_conversation_resolution = true
+ lock_branch                     = true
+}
+
+resource "github_branch_protection" "master" {
+ for_each = github_repository.default
+
+ repository_id = each.value.name
+ pattern       = var.branch_pattern[1]
+
+ required_pull_request_reviews {
+   required_approving_review_count = 2
+   require_code_owner_reviews      = true
+   require_last_push_approval      = true
+   #pull_request_bypassers = var.pull_request_bypassers
+ }
+
+ required_status_checks {
+   strict   = true
+   contexts = ["build-and-test-ubuntu"]
+ }
+
+ require_conversation_resolution = true
+ lock_branch                     = true
 }
 
 resource "github_repository_autolink_reference" "autolink" {
-  for_each = {
-    for p in setproduct([for key, repo in local.repos : key], local.jira_projects) : "${p[0]}/${p[1]}" => {
-      repo = p[0]
-      jira = p[1]
-    }
-  }
+ for_each = {
+   for p in setproduct([for key, repo in local.repos : key], local.jira_projects) : "${p[0]}/${p[1]}" => {
+     repo = p[0]
+     jira = p[1]
+   }
+ }
 
-  repository          = each.value.repo
-  key_prefix          = "${each.value.jira}-"
-  target_url_template = "https://dataflexnet.atlassian.net/browse/${each.value.jira}-<num>"
-  is_alphanumeric     = false
+ repository          = each.value.repo
+ key_prefix          = "${each.value.jira}-"
+ target_url_template = "https://dataflexnet.atlassian.net/browse/${each.value.jira}-<num>"
+ is_alphanumeric     = false
 
-  depends_on = [
-    github_repository.default
-  ]
+ depends_on = [
+   github_repository.default
+ ]
 }
--- variable.tf
+++ variable.tf
@@ -1,2 +1,6 @@
 variable "token" {}
 #variable "pull_request_bypassers" {}
+variable "branch_pattern"{
+    type = list(string)
+    default = ["planned-release-dev", "master"]
+}
